611d96a96cf7128e5e4291333c28b5781e62f2e2
liuyingjie1130 <1410505543@qq.com>
2018-05-22 21:41:01 +0800

    练习

--- LS25_/js25_.js
+++ LS25_/js25_.js
@@ -88,3 +88,180 @@
 
 // }
 
+
+
+// 2018.5.28
+// 冒泡和捕获的排序    默认false是冒泡
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click");
+    },false);//第3个参数可以不写，默认为false
+
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click");
+    },false);
+
+    document.addEventListener("click",function (e) {
+        console.log("document click");
+    },false);
+
+    document.body.addEventListener("click",function (e) {
+        console.log("body click");
+    },false);
+}//div2 click  div1 click body click document click
+
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click");
+    },false);
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click");
+    },false);
+    div3.addEventListener("click",function (e) {
+        console.log("div3 click");
+    },false);
+
+    document.addEventListener("click",function (e) {
+        console.log("document click");
+    },false);
+    document.body.addEventListener("click",function (e) {
+        console.log("body click");
+    },false);
+}
+//div3 click
+//  div2 click
+//  div1 click
+//  document click
+
+
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click");
+    },true);
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click");
+    },true);
+    div3.addEventListener("click",function (e) {
+        console.log("div3 click");
+    },true);
+
+    document.addEventListener("click",function (e) {
+        console.log("document click");
+    },false);
+    document.body.addEventListener("click",function (e) {
+        console.log("body click");
+    },false);
+}
+//div1 click
+// div2 click
+// div3 click
+// body click
+// document click
+
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click");
+    },true);
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click");
+    },true);
+    div3.addEventListener("click",function (e) {
+        console.log("div3 click");
+    },true);
+
+    document.addEventListener("click",function (e) {
+        console.log("document click");
+    },true);
+    document.body.addEventListener("click",function (e) {
+        console.log("body click");
+    },true);
+}
+//document click
+// body click
+// div1 click
+//  div2 click
+// div3 click
+
+
+//再谈事件对象与事件流
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click--red");
+        console.log("target:",e.target);
+        console.log("this:",this);
+        console.log(e.bubbles,e.cancelable,e.cancelBubble);//是否会冒泡   是否可以阻止默认事件   是否可以阻止冒泡，只有在IE中才可以
+        //e.stopPropagation();//阻止事件冒泡
+    },false);//改成true会怎样
+
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click--yellow");//
+        this.style.backgroundColor='black';
+        //e.stopPropagation();
+    },false);//改成true会怎样
+
+    div3.addEventListener("click",function (e) {
+        console.log("div3 click--blue");
+        //e.stopPropagation();
+    },false);//改成true会怎样
+
+    //阻止默认事件行为
+    document.getElementById("aId").addEventListener("click",function (e) {
+        e.preventDefault();//阻止默认事件，阻止了链接跳转
+        console.log("a click");
+    });
+}
+//div3 click--blue
+// div2 click--yellow
+// div1 click--red
+// target: <div id="div3"​</div>​
+// this: <div id=​"div1">​…​</div>​
+// true true false
+
+window.onload = function (e) {
+    var div1 = document.getElementById("div1");
+    var div2 = document.getElementById("div2");
+
+    div1.addEventListener("click",function (e) {
+        console.log("div1 click--red");
+        console.log("target:",e.target);
+        console.log("this:",this);
+        console.log(e.bubbles,e.cancelable,e.cancelBubble);//是否会冒泡   是否可以阻止默认事件   是否可以阻止冒泡只有在IE中才可以
+        //e.stopPropagation();//阻止事件冒泡
+    },true);
+
+    div2.addEventListener("click",function (e) {
+        console.log("div2 click--yellow");//
+        this.style.backgroundColor='black';
+        //e.stopPropagation();
+    },true);
+
+    div3.addEventListener("click",function (e) {
+        console.log("div3 click--blue");
+        //e.stopPropagation();
+    },true);
+
+    //阻止默认事件行为
+    document.getElementById("aId").addEventListener("click",function (e) {
+        e.preventDefault();//阻止默认事件，阻止了链接跳转
+        console.log("a click");
+    });
+}
+// div1 click--red
+// target: <div id=​"div3">​</div>​
+// this: <div id=​"div1">​…​</div>​
+// true true false
+// div2 click--yellow
+// div3 click--blue
+
